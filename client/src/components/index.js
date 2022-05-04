7641aca755ea29c5f974430772f986c149dcb022
Amrutansu Mishra <amrutansumishra@gmail.com>
2022-05-04 22:36:51 +0530

    First commit

---
+++ client/src/components/index.js
@@ -0,0 +1,20 @@
+import Navbar from './Navbar/Nav';
+import Footer from './Footer/Footer';
+import Register from './Register/Register';
+import Login from './Login/Login';
+import AddProduct from './Products/AddProduct';
+import UpdateProduct from './Products/UpdateProduct';
+import Home from './Home/Home';
+import Profile from './Profile/Profile';
+
+
+export {
+    Navbar,
+    Footer,
+    Register,
+    Login,
+    AddProduct,
+    UpdateProduct,
+    Home,
+    Profile
+}
